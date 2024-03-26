3fad0ace9e0319c5bc4f12446a11356693290dd6
ABid hasan <abeydhasan134@gmail.com>
2024-03-27 01:39:25 +0600

    Read Book sorted

--- src/ListedBook/components/listedBook.jsx
+++ src/ListedBook/components/listedBook.jsx
@@ -2,12 +2,33 @@ import React, { useContext, useEffect, useState } from "react";
 import NoteContext from "../../context/noteContext";
 import MarkedBook from "./markedBook";
 import WishMake from "./wishMake";
+import { IoChevronDown } from "react-icons/io5";
 
 const ListedBook = () => {
   const cardInfo = useContext(NoteContext);
   const [markedBooks, setMarkedBooks] = useState([]);
   const [markeWish, setMarkedWish] = useState([]);
-  
+  const [displayBook,setDisplayBook]=useState([]);
+
+  const handelShort = (etype) => {
+    if (etype === "all") {
+      setDisplayBook(markedBooks);
+      console.log("all")
+    } else if (etype === "page") {
+      const sortedBooks = [...markedBooks].sort((a, b) => a.totalPages - b.totalPages);
+      setDisplayBook(sortedBooks);
+    }
+    else if (etype === "rating") {
+      const sortedrat = [...markedBooks].sort((a, b) => b.rating - a.rating);
+      setDisplayBook(sortedrat);
+    }
+    else if (etype === "year") {
+      const sortedrat = [...markedBooks].sort((a, b) => a.yearOfPublishing - b.yearOfPublishing);
+      setDisplayBook(sortedrat);
+    }
+    // Add additional sorting logic for other criteria if needed
+  };
+
   useEffect(() => {
     const getFromLocal = JSON.parse(localStorage.getItem("wish-id")) || [];
     const filteredBooks = cardInfo.filter((book) =>
@@ -22,7 +43,9 @@ const ListedBook = () => {
       getFromLocal.includes(book.bookId)
     );
     setMarkedBooks(filteredBooks);
+    setDisplayBook(filteredBooks);
   }, [cardInfo]);
+  // console.log(displayBook);
 
   return (
     <div role="tablist" className="tabs tabs-lifted">
@@ -34,13 +57,36 @@ const ListedBook = () => {
         aria-label="Read Book"
         checked
       />
+
       <div
         role="tabpanel"
         className="tab-content bg-base-100 border-base-300 rounded-box p-6"
       >
-        {markedBooks.map((book) =><MarkedBook book={book}/>
-        )}
+        <div className="border-2 border-red-500 justify-center text-center">
+          <details className="dropdown">
+          <summary className="m-1 btn bg-green-500 hover:bg-green-600">sort by<IoChevronDown></IoChevronDown></summary>
+            <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+            <li>
+              <a onClick={()=>handelShort("all")}>All</a>
+            </li>
+            <li>
+              <a onClick={()=>handelShort("rating")}>Reating</a>
+            </li>
+            <li>
+              <a onClick={()=>handelShort("page")}>Number of Page</a>
+            </li>
+            <li>
+              <a onClick={()=>handelShort("year")}>Publishing Year</a>
+            </li>
+          </ul>
+          </details>
+        </div>
+
+        {displayBook.map((book) => (
+          <MarkedBook book={book} />
+        ))}
       </div>
+
       <input
         type="radio"
         name="my_tabs_2"
@@ -52,8 +98,9 @@ const ListedBook = () => {
         role="tabpanel"
         className="tab-content bg-base-100 border-base-300 rounded-box p-6"
       >
-        {markeWish.map((book) =><WishMake book={book}/>
-        )}
+        {markeWish.map((book) => (
+          <WishMake book={book} />
+        ))}
       </div>
     </div>
   );
--- src/ListedBook/components/markedBook.jsx
+++ src/ListedBook/components/markedBook.jsx
@@ -1,5 +1,9 @@
 import React, { useContext, useEffect, useState } from "react";
 // import NoteContext from '../../context/noteContext';
+import { IoLocationOutline } from "react-icons/io5";
+import { FaUsers } from "react-icons/fa6";
+import { FaRegFileLines } from "react-icons/fa6";
+import { IoChevronDown } from "react-icons/io5";
 
 const MarkedBook = ({ book }) => {
   console.log(book);
@@ -17,16 +21,49 @@ const MarkedBook = ({ book }) => {
   // }, [cardInfo]);
 
   return (
-    <div className="hero">
-  <div className="hero-content flex-col lg:flex-row">
-    <img src={book.image} />
     <div>
-      <h1 className="text-5xl font-bold">Box Office News!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary">Get Started</button>
+     
+      <div className="hero border-2 border-pink-100 rounded-md justify-start my-4">
+        <div className="hero-content flex-col lg:flex-row">
+          <img className="h-96" src={book.image} />
+          <div>
+            <h1 className="text-5xl font-bold">{book.bookName}</h1>
+            <p className="py-6">By:{book.author}</p>
+            {/* tags and location  */}
+            <div className="flex items-center gap-4 mb-6">
+              <p className="font-bold ">Tags:</p>
+              <p className="text-sky-400 shadow-sm shadow-green-100">
+                #{book.tags[0]}
+              </p>
+              <p className="text-sky-400 shadow-sm shadow-green-100">
+                #{book.tags[1]}
+              </p>
+              <div className="flex items-center ml-10">
+                <IoLocationOutline></IoLocationOutline>
+                <p>Year of publishing{book.yearOfPublishing}</p>
+              </div>
+            </div>
+            {/* tags and location end */}
+            <div className="flex items-center gap-3 text-2xl opacity-70">
+              <FaUsers></FaUsers>
+              <p>Publisher: {book.publisher}</p>
+              <FaRegFileLines className="ml-10"></FaRegFileLines>
+              <p>{book.totalPages}</p>
+            </div>
+            <hr className="border-1 border-gray-400 my-4 w-max-auto" />
+            <button className="btn bg-sky-100 text-sky-500 rounded-badge  mx-4">
+              Category: {book.category}
+            </button>
+            <button className="btn bg-orange-100 text-orange-500 rounded-badge mx-4">
+              Rating:{book.rating}
+            </button>
+            <button className="btn bg-green-500 text-white rounded-badge mx-4">
+              View Details
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
   );
 };
 
